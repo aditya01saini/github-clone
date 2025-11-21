604a768cb55fa9d2b53e8ea4bdcb1c3f87fee230
Dev aadi <adityasaini35947@gmail.com>
2025-11-21 23:45:12 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
+  .command("start, Start a new server", {} , startServer)
   .command("init", "Initialise a new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -56,3 +57,8 @@ yargs(hideBin(process.argv))
 
   .demandCommand(1, "You need at least one command")
   .help().argv;
+
+
+  function startServer(){
+    console.log("Server logic called!");
+  }
