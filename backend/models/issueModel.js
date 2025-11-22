e650a59ec5efcb26d1d41ad7db08fbc2c506835c
Dev aadi <adityasaini35947@gmail.com>
2025-11-22 23:44:05 +0530

    modified: userModel.js

---
+++ backend/models/issueModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { default: Repository } = require("./repoModel");
+const { required } = require("yargs");
+const { Schema } = mongoose;
+
+const IssueSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["open", "closed"],
+    default: "open",
+  },
+  repository: {
+    type: Schema.Types.ObjectId,
+    ref: "Repository",
+    required: true,
+  },
+});
+
+const Issue = mongoose.model("Issue", IssueSchema);
+
+export default Issue;
