5a5d68e63e8ff54cf60708ab0514650266169152
Dev aadi <adityasaini35947@gmail.com>
2025-11-25 23:35:49 +0530

    modified:   backend/routes/repo.router.js

--- backend/routes/repo.router.js
+++ backend/routes/repo.router.js
@@ -6,10 +6,9 @@ const repoRouter = express.Router();
 repoRouter.post("/repo/create", repoController.createRepository);
 repoRouter.get("/repo/all", repoController.getAllRepositories);
 repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
-repoRouter.get("/repo/:name", repoController.fetchRepositoryByName);
-repoRouter.get("/repo/:userID", repoController.fetchRepositoriesForCurrentUser);
-repoRouter.put("/repo/:id", repoController.updateRepositoryById);
-
-repoRouter.delete("/repo/:id", repoController.deleteRepositoryById);
+repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
+repoRouter.get("/repo/user/:userID", repoController.fetchRepositoriesForCurrentUser);
+repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
+repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
 repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById );
 module.exports = repoRouter;
